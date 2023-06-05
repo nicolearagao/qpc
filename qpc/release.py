a23fa79159f1483887a04509a0d9dee6c4f2fdfa
Nicole Aragao <nicolearagao@gmail.com>
2023-06-05 15:56:42 -0300

    feature: read env variable for package source info
    Package source env variable will have "qpc" as default if not set.

--- qpc/release.py
+++ qpc/release.py
@@ -1,9 +1,10 @@
 """File to hold release constants."""
+import os
 from . import __package__version__
 
 VERSION = __package__version__
 AUTHOR = "QPC Team"
 AUTHOR_EMAIL = "qpc@redhat.com"
-PKG_NAME = "qpc"
-ENTRYPOINT = f"{PKG_NAME}=qpc.__main__:main"
+QPC_VAR_PKG_SOURCE = os.environ.get("QPC_VAR_PKG_SOURCE", "qpc")
+ENTRYPOINT = f"{QPC_VAR_PKG_SOURCE}=qpc.__main__:main"
 URL = "https://github.com/quipucords/qpc"
